c8e19c240721db8b9e06f1d48389a26dae33a287
Nami Sunami <nsunami@pm.me>
2024-11-18 16:24:30 +0100

    feat(data): Add data representing the list of data stewards

---
+++ data/dataStewards.ts
@@ -0,0 +1,59 @@
+export const dataStewards = [
+  {
+    name: "Liz Guzman-Ramirez",
+    role: "Coordinator",
+    pureUrl: "https://research.tue.nl/en/persons/liz-guzman-ramirez",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/E6F3DA4B/0800161685d9da5130b9511cba31c392/image.png",
+  },
+  {
+    name: "Angela Aleksovska",
+    role: "EE",
+    pureUrl: "https://research.tue.nl/en/persons/angela-aleksovska",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/BB3A9314/ee39d529ae257a6908e3758531571254/image.png",
+  },
+  {
+    name: "Davide Nardi",
+    role: "M&CS",
+    pureUrl: "https://research.tue.nl/en/persons/davide-nardi",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/E82F130D/fa32dc3e0d3441ce1852d33fb8d51e19/image.png",
+  },
+  {
+    name: "Jay Nair",
+    role: "IE&IS",
+    pureUrl:
+      "https://research.tue.nl/en/persons/jayakrishnan-harikrishnavilas-raveendran-nair",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/BB86F741/93d2bfd61ff25a0a39b32e1676291921/image.png",
+  },
+  {
+    name: "Lucia Forrová",
+    role: "BE",
+    pureUrl: "https://research.tue.nl/en/persons/lucia-forrov%C3%A1",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/971233E4/457149811614201b7c24d74f26fda13f/image.png",
+  },
+  {
+    name: "Neda Nozouri",
+    role: "BmE, ME",
+    pureUrl: "https://research.tue.nl/en/persons/neda-norouzi",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/0975110E/8d60fc12cda6126569fccb399b7e47d4/image.png",
+  },
+  {
+    name: "Mariana Oshima Menegon",
+    role: "ID",
+    pureUrl: "https://research.tue.nl/en/persons/mariana-oshima-menegon",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/85AFDEB0/f81afc1780baf6f860eadf403b8bdf33/image.png",
+  },
+  {
+    name: "Nami Sunami",
+    role: "CE&C, APSE",
+    pureUrl: "https://research.tue.nl/en/persons/nami-sunami",
+    photoUrl:
+      "https://tracking.tue.nl/pic/acc/7713/A633B04A/1eea2ae7c4fb325dafcbfc26b0666199/image.png",
+  },
+]
