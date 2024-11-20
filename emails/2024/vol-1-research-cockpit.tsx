1780d8113127e6644c59fab289e7bdcc748be0c1
Nami Sunami <nsunami@pm.me>
2024-11-20 11:31:06 +0100

    feat: update content

--- emails/2024/vol-1-research-cockpit.tsx
+++ emails/2024/vol-1-research-cockpit.tsx
@@ -1,9 +1,11 @@
 import {
   Body,
+  Button,
   Column,
   Container,
   Head,
   Heading,
+  Hr,
   Html,
   Img,
   Link,
@@ -21,22 +23,74 @@ const baseUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : ""
 
-export const StripeWelcomeEmail = () => (
+type NewsletterEmailProps = {
+  unsubscribeUrl: string
+}
+
+export const NewsletterEmail = ({ unsubscribeUrl }: NewsletterEmailProps) => (
   <Tailwind>
     <Html className="font-sans">
       <Head />
-      <Preview>PAR Data Steward Newsletter Vol. 1 (2024-10-18)</Preview>
+      <Preview>PAR Data Steward Newsletter Vol. 1</Preview>
       <Body>
+        <Container>
+          <Section className="bg-fuchsia-800 text-sm px-4 py-[20px]">
+            <Row>
+              <Column>
+                <Link href="https://www.tue.nl/en/our-university/library/library-for-researchers-and-phds/research-data-management">
+                  <Img
+                    src={`${baseUrl}/static/TUe-logo-descriptor-line-white.png`}
+                    width={150}
+                    height={41}
+                    alt="TU/e Logo"
+                  />
+                </Link>
+              </Column>
+
+              <Column className="text-right">
+                <Link
+                  className="text-white [text-decoration:none]"
+                  href="https://www.tue.nl/en/our-university/library/library-for-researchers-and-phds/research-data-management"
+                >
+                  Web
+                </Link>
+              </Column>
+              <Column className="text-right pl-4">
+                <Link
+                  className="text-white [text-decoration:none] line-clamp-1"
+                  href="https://tuenl.sharepoint.com/sites/intranet-LIS/SitePages/Data-stewards(1).aspx"
+                >
+                  Intranet
+                </Link>
+              </Column>
+            </Row>
+          </Section>
+        </Container>
         <Container className="bg-fuchsia-800 text-white px-3">
           <Section>
-            <Text>PAR Data Steward Newsletter October 2024</Text>
-            <Img
-              src={`${baseUrl}/static/TUe-logo-descriptor-line-white.png`}
-              width={150}
-              height={41}
-              alt="TU/e Logo"
-            />
+            <Heading className="text-center">
+              PAR Data Steward Newsletter
+            </Heading>
           </Section>
+          <Section>
+            <Text>Dear Colleague,</Text>
+            <Text>
+              Welcome to the one and only newsletter dedicated exclusively to
+              researchers at TU/e!
+            </Text>
+            <Text>
+              In this Newsletter you will find news, updates, and services
+              strictly targeted to researchers. We plan to send updates twice
+              per year only.
+            </Text>
+            <Text>We hope you enjoy it,</Text>
+            <Text>
+              Research Data Stewards <br />
+              Product Area Research <br />
+              Library & Information Services
+            </Text>
+          </Section>
+          <Hr />
           <Section className="text-center">
             <Heading>Launch of the Research Cockpit</Heading>
             <Img
@@ -52,6 +106,12 @@ export const StripeWelcomeEmail = () => (
               additional products and services being introduced over time to
               support the entire research data lifecycle.
             </Text>
+            <Button
+              className="bg-white box-border rounded-md py-3 px-4 text-center font-semibold w-full"
+              href="https://tue.atlassian.net/helpcenter/research/"
+            >
+              Go to Research Cockpit
+            </Button>
           </Section>
           <Section>
             <Heading as="h2">Meet your data stewards</Heading>
@@ -79,10 +139,93 @@ export const StripeWelcomeEmail = () => (
               </Row>
             ))}
           </Section>
+          <Section>
+            <Heading as="h2">What can we help you with?</Heading>
+            <Section className="px-3">
+              <Row className="h-12">
+                <Column className="w-1/2">
+                  Funder requirements related to RDM
+                </Column>
+                <Column className="w-1/2">
+                  Research data storage solutions
+                </Column>
+              </Row>
+              <Row className="h-12">
+                <Column className="w-1/2">Publishing data & software</Column>
+                <Column className="w-1/2">
+                  Ethics applications & Informed consent forms
+                </Column>
+              </Row>
+              <Row className="h-12">
+                <Column className="w-1/2">Implementation of FAIR data</Column>
+                <Column>Privacy questions</Column>
+              </Row>
+              <Row className="h-12">
+                <Column className="w-1/2">Data Management Plans</Column>
+                <Column className="w-1/2">
+                  Legal agreements and Impact assessments
+                </Column>
+              </Row>
+            </Section>
+            <Button
+              className="bg-white box-border rounded-md py-3 px-4 text-center font-semibold w-full my-4"
+              href="https://www.tue.nl/en/our-university/library/library-for-researchers-and-phds/research-data-management"
+            >
+              Read more
+            </Button>
+          </Section>
+          <Section>
+            <Heading as="h2">Trivia from Data Stewards</Heading>
+            <Row>
+              <Column className="w-1/2">
+                <Text>
+                  Did you know that when our Sun starts to die, for a brief
+                  period of time (in astronomical time scales), it will become a
+                  Planetary Nebula?
+                </Text>
+                <Text>
+                  We think it will look something like this, with the outer
+                  layers reaching the orbit of the Earth.
+                </Text>
+                <Text>Liz (astrophysics)</Text>
+              </Column>
+              <Column className="w-1/2 text-center">Image here</Column>
+            </Row>
+          </Section>
+        </Container>
+        <Container className="my-4">
+          <Text className="text-center text-gray-700">
+            <Link href="https://tue.atlassian.net/helpcenter/research/">
+              Research Cockpit
+            </Link>{" "}
+            |{" "}
+            <Link href="https://openpar.pages.tue.nl/solution-searcher/index.html">
+              Solution Searcher
+            </Link>{" "}
+            |{" "}
+            <Link href="https://www.tue.nl/en/our-university/library/library-for-researchers-and-phds/research-data-management">
+              RDM TU/e Public Website
+            </Link>
+          </Text>
+          <Section className="text-center">
+            <Link href="https://zenodo.org/communities/tue_rdmsupport/records?q=&l=list&p=1&s=10&sort=newest">
+              <Img
+                className="mx-auto"
+                src="https://about.zenodo.org/static/img/logos/zenodo-black-200.png"
+              />
+            </Link>
+          </Section>
+          <Text className="text-center">
+            CC0 No Rights Reserved <br />
+            Public Domain Dedication 🧡
+          </Text>
+          <Text className="text-center">
+            <Link href={unsubscribeUrl || "#"}>Unsubscribe</Link>
+          </Text>
         </Container>
       </Body>
     </Html>
   </Tailwind>
 )
 
-export default StripeWelcomeEmail
+export default NewsletterEmail
